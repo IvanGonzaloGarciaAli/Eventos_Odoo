af7459d3eb828be058a863bcf7dc41122585f836
IvanGonzaloGarciaAli <ivan.gonzalo.garcia.ali1@gmail.com>
2025-02-17 10:37:31 +0100

    He modificado la variable de nombre de evento por name, ya que me daba error y era porque al principio le puse de nombre name y no puedo cambiarlo.Tambien he añadido el modelaccess de tarea

--- odoo/addons/igga_eventos/models/evento.py
+++ odoo/addons/igga_eventos/models/evento.py
@@ -5,7 +5,7 @@ class Evento(models.Model):
     _name = 'igga_eventos.evento'
     _description = 'Evento'
 
-    nombre = fields.Char(string="Nombre del Evento", required=True)
+    name = fields.Char(string="Nombre del Evento", required=True)
     fecha = fields.Date(string="Fecha del Evento", required=True)
     lugar = fields.Char(string="Lugar del Evento", required=True)
     estado = fields.Selection([
