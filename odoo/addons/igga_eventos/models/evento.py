173b4d881992b4d16a6c7f91fa279553a330d5d8
IvanGonzaloGarciaAli <ivan.gonzalo.garcia.ali1@gmail.com>
2025-02-21 12:29:02 +0100

    He añadido cosas a Evento.py,Tarea.py y a sus respectivos xml. Evento.py: - He añadido el campo cliente_id que es un res_user - He añadido tarea_id que es un One2Many de la relacion evento_id en igga_eventos.tarea. Tarea.py: - He añadido responsable_id que es un res_user - He añadido evento_id que es un Many2One de igga_eventos.evento
    Tambien he añadiod los campos a los xml de ambas clases:
    - Tree y Form de tarea
    - Tree de evento ya que no tengo todavia el form

--- odoo/addons/igga_eventos/models/evento.py
+++ odoo/addons/igga_eventos/models/evento.py
@@ -8,9 +8,11 @@ class Evento(models.Model):
     name = fields.Char(string="Nombre del Evento", required=True)
     fecha = fields.Date(string="Fecha del Evento", required=True)
     lugar = fields.Char(string="Lugar del Evento", required=True)
+    cliente_id = fields.Many2one('res.partner', string="Cliente", required=True)
     estado = fields.Selection([
         ('planificacion', 'Planificacion'),
         ('en_curso', 'En curso'),
         ('finalizado', 'Finalizado')
     ], string="Estado", default='planificacion')
     imagen = fields.Binary(string="Imagen del Evento")
+    tarea_ids = fields.One2many('igga_eventos.tarea', 'evento_id', string="Tareas")
--- odoo/addons/igga_eventos/models/tarea.py
+++ odoo/addons/igga_eventos/models/tarea.py
@@ -6,8 +6,10 @@ class Tarea(models.Model):
     _description = 'Tarea'
 
     descripcion = fields.Text(string="Descripcion", required=True)
+    responsable_id = fields.Many2one('res.users', string="Responsable", required=True)
     fecha_limite = fields.Date(string="Fecha de limite", required=True)
     estado = fields.Selection([
         ('pendiente', 'Pendiente'),
         ('completada', 'Completada')
     ], string="Estado", default='pendiente')
+    evento_id = fields.Many2one('igga_eventos.evento', string="Evento", required=True)
