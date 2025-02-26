607cfbda1a3aa8a9892c2a4970aa26381e72e972
IvanGonzaloGarciaAli <ivan.gonzalo.garcia.ali1@gmail.com>
2025-02-26 13:20:48 +0100

    He añadido el security y he borrado libreria

--- odoo/addons/igga_eventos/__manifest__.py
+++ odoo/addons/igga_eventos/__manifest__.py
@@ -22,7 +22,7 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/evento.xml',
         'views/menus.xml',
         'views/tarea.xml',
